bf2737da5ee4a7e740b0747237dc8a81bddc4f65
Danial Chitnis <danchitnis@users.noreply.github.com>
2019-10-09 01:34:41 +0100

    init

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/script.js',
+  mode: 'development',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
